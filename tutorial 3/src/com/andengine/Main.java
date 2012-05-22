a54b6e140070a05b037561583f3a7d2c20d9e618
Niranjan R <nija1993@gmail.com>
2012-05-22 16:00:39 +0530

    added tutorial 3

---
+++ tutorial 3/src/com/andengine/Main.java
@@ -0,0 +1,83 @@
+package com.andengine;
+
+import org.anddev.andengine.engine.Engine;
+import org.anddev.andengine.entity.scene.Scene;
+import org.anddev.andengine.entity.scene.Scene.IOnSceneTouchListener;
+import org.anddev.andengine.opengl.texture.Texture;
+import org.anddev.andengine.opengl.texture.TextureOptions;
+import org.anddev.andengine.opengl.texture.region.TextureRegion;
+import org.anddev.andengine.opengl.texture.region.TextureRegionFactory;
+import org.anddev.andengine.ui.activity.BaseGameActivity;
+import org.anddev.andengine.engine.camera.ZoomCamera;
+import org.anddev.andengine.engine.options.EngineOptions;
+import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
+import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.anddev.andengine.entity.scene.background.ColorBackground;
+import org.anddev.andengine.entity.sprite.Sprite;
+import org.anddev.andengine.entity.util.FPSLogger;
+import org.anddev.andengine.input.touch.TouchEvent;
+import org.anddev.andengine.input.touch.detector.ScrollDetector;
+import org.anddev.andengine.input.touch.detector.ScrollDetector.IScrollDetectorListener;
+import org.anddev.andengine.input.touch.detector.SurfaceScrollDetector;
+
+import android.util.Log;
+
+public class Main extends BaseGameActivity implements IScrollDetectorListener, IOnSceneTouchListener {
+	
+	static final int CAMERA_WIDTH=480;
+	static final int CAMERA_HEIGHT=320;
+	
+	private static final String TAG = "AndEngineTest";
+	
+	private ZoomCamera mCamera;
+	private Texture mTexture;
+	private TextureRegion mFaceTextureRegion;
+	private SurfaceScrollDetector mScrollDetector;
+	
+    public void onLoadComplete(){
+    	// TODO Auto-generated method stub
+    }
+    
+    public Engine onLoadEngine(){
+    	this.mCamera = new ZoomCamera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+    	final int alturaTotal = CAMERA_HEIGHT*3;
+    	this.mCamera.setBounds(0, CAMERA_WIDTH, 0, alturaTotal);
+    	this.mCamera.setBoundsEnabled(true);
+    	return new Engine(new EngineOptions(true, ScreenOrientation.LANDSCAPE, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), this.mCamera));
+    }
+    
+    public void onLoadResources(){
+    	this.mTexture = new Texture(64, 64, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+    	this.mFaceTextureRegion = TextureRegionFactory.createFromAsset(this.mTexture, this, "gfx/ui_ball_1.png", 0, 0);
+    	this.mEngine.getTextureManager().loadTexture(this.mTexture);
+    }
+    
+    public Scene onLoadScene(){
+    	this.mEngine.registerUpdateHandler(new FPSLogger());
+    	final Scene scene = new Scene(1);
+    	scene.setOnAreaTouchTraversalFrontToBack();
+    	final int centerX = (CAMERA_WIDTH - this.mFaceTextureRegion.getWidth()) / 2;
+    	final int centerY = (CAMERA_HEIGHT - this.mFaceTextureRegion.getHeight()) / 2;
+    	
+    	this.mScrollDetector = new SurfaceScrollDetector(this);
+    	this.mScrollDetector.setEnabled(true);
+    	
+    	final Sprite ball = new Sprite(centerX, centerY, this.mFaceTextureRegion);
+    	scene.getLastChild().attachChild(ball);
+    	
+    	scene.setOnSceneTouchListener(this);
+    	scene.setTouchAreaBindingEnabled(true);
+    	
+    	return scene;
+    }    
+    
+    public void onScroll(ScrollDetector pScollDetector, TouchEvent pTouchEvent,
+    float pDistanceX, float pDistanceY) {
+    	this.mCamera.offsetCenter(-pDistanceX, -pDistanceY);  
+    	}
+    		 
+    public boolean onSceneTouchEvent(Scene pScene, TouchEvent pSceneTouchEvent) {
+    	this.mScrollDetector.onTouchEvent(pSceneTouchEvent);
+    	return true;
+    	}
+}
---
+++ tutorial 3/src/com/andengine/Splash.java
@@ -0,0 +1,39 @@
+package com.andengine;
+
+import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
+import org.anddev.andengine.opengl.texture.source.AssetTextureSource;
+import org.anddev.andengine.opengl.texture.source.ITextureSource;
+import org.anddev.andengine.ui.activity.BaseSplashActivity;
+
+import android.app.Activity;
+
+public class Splash extends BaseSplashActivity {
+	
+	private static final int SPLASH_DURATION=5;
+	private static final float SPLASH_SCALE_FROM = 1f;
+
+	@Override
+	protected ScreenOrientation getScreenOrientation() {
+		return ScreenOrientation.LANDSCAPE;
+	}
+
+	@Override
+	protected ITextureSource onGetSplashTextureSource() {
+		return new AssetTextureSource(this, "gfx/Welcome.png");
+	}
+
+	@Override
+	protected float getSplashDuration() {
+		return SPLASH_DURATION;
+	}
+
+	@Override
+	protected Class<? extends Activity> getFollowUpActivity() {
+		return Main.class;
+	}
+	
+	protected float getSplashScaleFrom(){
+		return SPLASH_SCALE_FROM;
+	}
+
+}
